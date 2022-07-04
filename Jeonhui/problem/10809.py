52866afc0b58d85f592afee1a139237d23a902f3
SarahParkSehyun <sarah1918@naver.com>
2022-07-04 22:53:29 +0900

    풀어따

---
+++ Jeonhui/problem/10809.py
@@ -0,0 +1,12 @@
+result=[]
+alph=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+s=input()
+for i in range(26):
+    if alph[i] in s:
+        pos=s.find(alph[i])
+        result.append(pos)
+    else:
+        result.append(-1)
+
+for j in range(26):
+    print(result[j],end=' ')
