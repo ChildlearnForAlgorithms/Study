8a079e99a783245b95df59692363f4b0341dd5e9
Jeonhui <48307153+Jeonhui@users.noreply.github.com>
2022-07-08 16:49:30 +0900

    Update 2908.py

--- Sehyun/problem/2908.py
+++ Sehyun/problem/2908.py
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 import sys
 a= sys.stdin.readline().split()
 print(a[0][::-1] if int(a[0][::-1]) > int(a[1][::-1]) else a[1][::-1])
-=======
 a,b=input().split()
 a_list=list(a)
 b_list=list(b)
@@ -11,5 +9,3 @@ b_list.reverse()
 a2=''.join(a_list)
 b2=''.join(b_list)
 print(max(a2,b2))
-#커밋
->>>>>>> cec8d9b82b4579bbbcd8ccb3f32835ec58b6b308
