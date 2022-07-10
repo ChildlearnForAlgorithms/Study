dfc899e0e447492da88a9de7ffc0c8f1ab4c5856
SarahParkSehyun <sarah1918@naver.com>
2022-07-11 02:58:17 +0900

    후우

--- Sehyun/problem/2941.py
+++ Sehyun/problem/2941.py
@@ -1,13 +1,12 @@
 s=input()
 cralph=['c=','c-','dz=','d-','lj','nj','s=','z=']
 count=0
+
 for i in range(len(cralph)):
     for j in range(len(s)-len(cralph[i])+1):
         if cralph[i] in s[j:j+len(cralph[i])]:
             count += s.count(cralph[i])
             s=s.replace(s[j:j+len(cralph[i])]," ")
-            #print(s)
-            #print(count)
 
 s=s.replace(" ","")
 count+=len(s)
