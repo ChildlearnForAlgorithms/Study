3ebd2bb8dfbce63fdfd60ddd38212d00e157b0f8
SarahParkSehyun <sarah1918@naver.com>
2022-07-29 15:03:03 +0900

    생일생일

--- Sehyun/구름.py
+++ Sehyun/구름.py
@@ -1,45 +1,13 @@
-class Heap:
-    def __init__(self, L=[]):
-        self.A=L
-        self.make_heap()
-
-    def __str__(self):
-        return str(self.A)
-
-    def heapify_down(self, k, n):
-        while 2*k+1<n:
-            left,right=2*k+1,2*k+2
-            if left<n and self.A[left]>self.A[k]:
-                m=left
-            else:
-                m=k
-            if right<n and self.A[right]>self.A[m]:
-                m=right
-            if m!=k:
-                self.A[m],self.A[k]=self.A[k],self.A[m]
-                k=m
-            else:
-                break
+n = int(input())
+A = list(map(int, input().split()))
 
 
+def max_interval_DP(A):
+    S = [0] * len(A)
+    S[0] = A[0]
+    for i in range(1, len(A)):
+        S[i] = max(S[i - 1] + A[i], A[i])
+    return S.index(max(S))
 
 
-    def make_heap(self,):
-        n=len(self.A)
-        for i in range(n-1,-1,-1):
-            self.heapify_down(i,n)
-
-    def heap_sort(self):
-        n=len(L)
-        for i in range(n-1,-1,-1):
-            self.A[0],self.A[i]=self.A[i],self.A[0]
-            n=n-1
-            self.heapify_down(0,n)
-
-
-
-
-L = [int(x) for x in input().split()]
-H = Heap(L)
-H.heap_sort()
-print(H)
+print(max_interval_DP(A))
