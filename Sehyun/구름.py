5692ce4c7e581141afbe7572fec6c780809c72d1
SarahParkSehyun <sarah1918@naver.com>
2022-08-02 19:25:05 +0900

    오백완

--- Sehyun/구름.py
+++ Sehyun/구름.py
@@ -1,13 +1,25 @@
-n = int(input())
-A = list(map(int, input().split()))
+n,t=map(int,input().split())
+r,c,d=input().split()
+r,c=int(r)-1,int(c)-1
 
+dxs, dys = [0, 1, -1, 0], [1, 0, 0, -1]
+dir_num=0
+if d=='L':
+    dir_num=3
+elif d=='R':
+    dir_num=0
+elif d=='U':
+    dir_num=2
+elif d=='D':
+    dir_num=1
 
-def max_interval_DP(A):
-    S = [0] * len(A)
-    S[0] = A[0]
-    for i in range(1, len(A)):
-        S[i] = max(S[i - 1] + A[i], A[i])
-    return S.index(max(S))
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
 
-
-print(max_interval_DP(A))
+for _ in range(t):
+    nx,ny=r+dxs[dir_num],c+dys[dir_num]
+    if in_range(nx,ny):
+        r,c=nx,ny
+    else:
+        dir_num=3-dir_num
+print(r+1,c+1)
