9b494fe9906342c0f13a8bbe417d9d5de1a5f07f
SarahParkSehyun <sarah1918@naver.com>
2022-08-03 22:11:50 +0900

    코드트리

--- Sehyun/구름.py
+++ Sehyun/구름.py
@@ -1,25 +1,17 @@
-n,t=map(int,input().split())
-r,c,d=input().split()
-r,c=int(r)-1,int(c)-1
+import math
 
-dxs, dys = [0, 1, -1, 0], [1, 0, 0, -1]
-dir_num=0
-if d=='L':
-    dir_num=3
-elif d=='R':
-    dir_num=0
-elif d=='U':
-    dir_num=2
-elif d=='D':
-    dir_num=1
+def matrix_mult():
+    for i in range(n):
+    	for j in range(n):
+            C[i][j] = math.inf # math module에서 제공하는 매우 큰 정수
+            for k in range(i, j):
+                cost = min(C[i][k]+C[k+1][j]+P[i]*P[k+1]*P[j+1])
+                if cost<C[i][j]:
+                    C[i][j] = cost
+    return C[1][n-1]
 
-def in_range(x, y):
-    return 0 <= x and x < n and 0 <= y and y < n
-
-for _ in range(t):
-    nx,ny=r+dxs[dir_num],c+dys[dir_num]
-    if in_range(nx,ny):
-        r,c=nx,ny
-    else:
-        dir_num=3-dir_num
-print(r+1,c+1)
+n = int(input()) # n = 행렬 갯수, M_0부터 행렬시작임을 주의!
+P = [int(x) for x in input().split()] # M_i = p_i x p_{i+1}
+C = [[0]*n for _ in range(n)] # 비용을 저장할 2차원 리스트 C 초기화
+min_cost = matrix_mult()
+print(min_cost)
