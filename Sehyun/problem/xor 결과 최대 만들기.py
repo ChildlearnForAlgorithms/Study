e57858c07c6a9af261e89631fb1140fcfc9ccf3b
SarahParkSehyun <sarah1918@naver.com>
2022-08-10 15:13:59 +0900

    아돈노,,

---
+++ Sehyun/problem/xor 결과 최대 만들기.py
@@ -0,0 +1,17 @@
+n,m=map(int,input().split())
+lst=list(map(int,input().split()))
+ans=0
+
+def find_max_xor(curr_idx,cnt,curr_val):
+    global ans
+
+    if cnt==m:
+        ans=max(ans,curr_val)
+        return
+    if curr_idx==n:
+        return
+    find_max_xor(curr_idx+1,cnt+1,curr_val^lst[curr_idx])
+    find_max_xor(curr_idx + 1, cnt, curr_val)
+
+find_max_xor(0,0,0)
+print(ans)
