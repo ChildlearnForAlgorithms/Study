e12b1ed6f357b0df5e9488818137d8fcd6d46c18
SarahParkSehyun <sarah1918@naver.com>
2022-08-12 00:31:54 +0900

    코드트리

---
+++ Sehyun/problem/최소 경로로 탈출 하기.py
@@ -0,0 +1,29 @@
+from collections import deque
+n,m=map(int,input().split())
+grid=[list(map(int,input().split())) for _ in range(n)]
+
+q=deque()
+step=[[0]*m for _ in range(n)]
+visited=[[False]*m for _ in range(n)]
+
+def push(x,y,s):
+    step[x][y]=s
+    visited[x][y]=True
+    q.append((x,y))
+def in_range(x,y):
+    return 0<=x and x<n and 0<=y and y<m
+def bfs():
+    dxs,dys=[-1,1,0,0],[0,0,-1,1]
+    while q:
+        x,y=q.popleft()
+        for dx,dy in zip(dxs,dys):
+            nx,ny=x+dx,y+dy
+            if in_range(nx,ny) and not visited[nx][ny] and grid[nx][ny]:
+                push(nx,ny,step[x][y]+1)
+push(0,0,0)
+bfs()
+
+if visited[n-1][m-1]:
+    print(step[n-1][m-1])
+else:
+    print(-1)
--- Sehyun/problem/코드트리 연습.py
+++ Sehyun/problem/코드트리 연습.py
@@ -1,26 +1,29 @@
-n=int(input())
-answer=[]
-visited=[False]*(n+1)
+n,m=map(int,input().split())
+grid=[
+    list(map(int,input().split()))
+    for _ in range(m)]
 
-def print_answer():
-    for element in answer:
-        print(element,end=' ')
-    print()
-    return
+visited=[[0]*m for _ in range(n)]
 
-def choose(curr_num):
-    if curr_num==n+1:
-        print_answer()
-        return
-    for i in range(1,n+1):
-        if visited[i]:
-            continue
-        visited[i]=True
-        answer.append(i)
+def in_range(x,y):
+    return 0<=x and x<n and 0<=y and y<m
 
-        choose(curr_num+1)
-        
-        answer.pop()
-        visited[i]=False
+def can_go(x,y):
+    if not in_range(x,y):
+        return False
+    if visited[x][y] or grid[x][y]==0:
+        return False
+    return True
 
-choose(1)
+def dfs(x,y):
+    dxs,dys=[1,0],[0,1]
+    for dx,dy in zip(dxs,dys):
+        nx,ny=x+dx,y+dy
+        if can_go(nx,ny):
+            visited[nx][ny]=1
+            dfs(nx,ny)
+
+
+visited[0][0]=1
+dfs(0,0)
+print(visited[n-1][m-1])
