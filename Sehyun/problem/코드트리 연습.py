954ce31c6629168ff22f032eab902bebf2a7cda8
SarahParkSehyun <sarah1918@naver.com>
2022-08-12 18:07:35 +0900

    코드트리

--- Sehyun/problem/코드트리 연습.py
+++ Sehyun/problem/코드트리 연습.py
@@ -1,29 +1,8 @@
-n,m=map(int,input().split())
-grid=[
-    list(map(int,input().split()))
-    for _ in range(m)]
+n=int(input())
+dp=[0]*(46)
+dp[1]=1
+dp[2]=1
+for i in range(3,n+1):
+    dp[i]=dp[i-1]+dp[i-2]
 
-visited=[[0]*m for _ in range(n)]
-
-def in_range(x,y):
-    return 0<=x and x<n and 0<=y and y<m
-
-def can_go(x,y):
-    if not in_range(x,y):
-        return False
-    if visited[x][y] or grid[x][y]==0:
-        return False
-    return True
-
-def dfs(x,y):
-    dxs,dys=[1,0],[0,1]
-    for dx,dy in zip(dxs,dys):
-        nx,ny=x+dx,y+dy
-        if can_go(nx,ny):
-            visited[nx][ny]=1
-            dfs(nx,ny)
-
-
-visited[0][0]=1
-dfs(0,0)
-print(visited[n-1][m-1])
+print(dp[n])
