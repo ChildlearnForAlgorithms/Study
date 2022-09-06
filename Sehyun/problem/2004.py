eee99efcae50f11958a41830edcfe4b318a87a3e
SarahParkSehyun <sarah1918@naver.com>
2022-09-06 20:10:24 +0900

    오백완

---
+++ Sehyun/problem/2004.py
@@ -0,0 +1,12 @@
+n,m=map(int,input().split())
+
+def count(n,k):
+    num=0
+    while n!=0:
+        n=n//k
+        num+=n
+    return num
+
+ans=min(count(n,2)-count(n-m,2)-count(m,2),count(n,5)-count(n-m,5)-count(m,5))
+
+print(ans)
