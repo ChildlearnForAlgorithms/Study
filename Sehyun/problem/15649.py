c348fff3085587d16bf4e6ba5622761ab4729c89
박세현 <sarah1918@naver.com>
2022-09-09 23:45:54 +0900

    오백완

---
+++ Sehyun/problem/15649.py
@@ -0,0 +1,20 @@
+n,m=map(int,input().split())
+nums=[]
+
+def print_permutation():
+    for num in nums:
+        print(num,end=' ')
+    print()
+
+def find_permutation(cnt):
+    if cnt==m:
+        print_permutation()
+        return
+    for i in range(1,n+1):
+        if i not in nums:
+            nums.append(i)
+            find_permutation(cnt+1)
+            nums.pop()
+
+find_permutation(0)
+
