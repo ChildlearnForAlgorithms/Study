4a8b312f462a3669d7b8a59791fb52abcf11b25b
박세현 <sarah1918@naver.com>
2022-09-16 20:02:15 +0900

    오백완

--- Sehyun/problem/1932.py
+++ Sehyun/problem/1932.py
@@ -11,9 +11,7 @@ dp[0][0]=lst[0][0]
 
 for i in range(1,n):
     dp[i][0]=lst[i][0]+dp[i-1][0]
-
-for i in range(1,n):
-    dp[i][i]=lst[i][-1]+dp[i-1][i-1]
+    dp[i][i] = lst[i][-1] + dp[i - 1][i - 1]
 
 for i in range(2,n):
     for j in range(i-1,0,-1):
