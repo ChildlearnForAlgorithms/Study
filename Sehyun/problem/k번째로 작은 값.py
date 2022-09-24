febe01eac3625fccf7de24282e2109e591ab912f
박세현 <sarah1918@naver.com>
2022-09-24 22:20:51 +0900

    과제 수정

--- Sehyun/problem/k번째로 작은 값.py
+++ Sehyun/problem/k번째로 작은 값.py
@@ -1,32 +1,20 @@
 import sys,heapq
 
-def find(A,k):
-    for i in range(k-1):
-        A[0]=A[-1]
-        A.pop()
-        m=0
-        while 2*m+1<len(A):
-            left,right=2*m+1,2*m+2
-            smaller=left
-            if right<len(A):
-                if A[right]<A[left]:
-                    smaller=right
-            if A[m]>A[smaller]:
-                A[m],A[smaller]=A[smaller],A[m]
-                m=smaller
-            else:
-                break
-    return A[0]
-
 A=list(map(int,sys.stdin.readline().split()))
 n=len(A)
-
+heap=[]
 m=[-sys.maxsize-1]*n
+m[0]=A[0]
 
-for i in range(n):
-    sliced_A=A[:i+1]
-    heapq.heapify(sliced_A)
+for i in range(1,n):
+    for j in range(0,i+1):
+        heapq.heappush(heap,A[j])
     k=i//3+1
-    m[i]=find(sliced_A,k)
+    for l in range(k-1):
+        heapq.heappop(heap)
+    a=heapq.heappop(heap)
+    m[i]=a
+    heap=[]
 
 print(sum(m))
+
