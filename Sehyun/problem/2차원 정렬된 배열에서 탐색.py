c4baae3f7bbbe8996aa329c2836cffe584d65a56
박세현 <sarah1918@naver.com>
2022-10-14 00:23:33 +0900

    .

--- Sehyun/problem/2차원 정렬된 배열에서 탐색.py
+++ Sehyun/problem/2차원 정렬된 배열에서 탐색.py
@@ -1,37 +1,41 @@
 import sys
 
-n,k=map(int,sys.stdin.readline().split())
+n, k = map(int, sys.stdin.readline().split())
 
-lst=[
-    list(map(int,sys.stdin.readline().split()))
+lst = [
+    list(map(int, sys.stdin.readline().split()))
     for _ in range(n)
 ]
 
-bool=False
+bool = False
 
-def binary_search(A,k):
+
+def binary_search(A, k):
     global bool
-    l,r=0,n
-    while r-l>=0:
-        m=(l+r)//2
-        if A[m]>k:
-            r=m-1
-        elif A[m]<k:
-            l=m+1
-        elif A[m]==k:
-            bool=True
+    l, r = 0, n - 1
+    while r - l >= 0:
+        m = (l + r) // 2
+        if A[m] > k:
+            r = m - 1
+        elif A[m] < k:
+            l = m + 1
+        elif A[m] == k:
+            bool = True
             return m
 
 
-m=0
+m = 0
 for i in range(n):
-    A=lst[i]
+    A = lst[i]
     for j in range(n):
-        m=binary_search(A,k)
-    if bool==False:
+        m = binary_search(A, k)
+    if bool == False:
         continue
-    elif bool==True:
+    elif bool == True:
         print((i, m))
         break
-if bool==False:
+
+if bool == False:
     print((-1, -1))
+
+
