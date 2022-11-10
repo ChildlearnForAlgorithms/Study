c2c4c94b5f1cba43a19acbcbd94852f352254451
박세현 <sarah1918@naver.com>
2022-11-11 02:14:47 +0900

    .

---
+++ Sehyun/problem/왼쪽 맞춤.py
@@ -0,0 +1,6 @@
+W=int(input())
+words=input().split()
+print(words)
+
+dp=[0]*(len(words)+1)
+
---
+++ Sehyun/problem/자리 값 합이 같은 수.py
@@ -0,0 +1,21 @@
+def solve(L, S):
+    dp = [[0] * (1001) for _ in range(1001)]
+    for j in range(1,10):
+        dp[1][j]=1
+    for i in range(1,10):
+        dp[i][1]=1
+    for i in range(2, 1001):
+        for j in range(2, 10):
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    for i in range(2,1001):
+        for j in range(10,1001):
+            sum=0
+            for k in range(j-9,j+1):
+                sum+=dp[i-1][k]
+            dp[i][j]=sum
+    return dp[L][S]
+
+
+L, S = [int(x) for x in input().split()]
+
+print(solve(L, S) % 2147483647+(19-S))
