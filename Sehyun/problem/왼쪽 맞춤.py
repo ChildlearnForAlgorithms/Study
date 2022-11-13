d4341e3eab06c8cddf5d24fb471d63583a306713
박세현 <sarah1918@naver.com>
2022-11-13 23:16:24 +0900

    .

--- Sehyun/problem/왼쪽 맞춤.py
+++ Sehyun/problem/왼쪽 맞춤.py
@@ -1,22 +1,21 @@
 W = int(input())
 words = input().split()
 # code below
-dp = [0 for _ in range(len(words))]
-dp[0] = (W - len(words[0])) ** 3
+dp = [0 for _ in range(len(words))]  # words 리스트 길이만큼 dp 배열을 생성해줌
+dp[0] = (W - len(words[0])) ** 3  # 첫 번째 단어의 penalty 값을 저장해줌
 
 for i in range(1, len(words)):
+    min_penalty = dp[i - 1] + (W - len(words[i])) ** 3
     word_length = len(words[i])
-    min_penalty = dp[i - 1] + (W - word_length) ** 3
     for j in range(i, 0, -1):
-        word_length += len(words[j - 1]) + 1
-        if word_length > W:
+        word_length += len(words[j - 1]) + 1  # 공백을 포함하기 위해 +1
+        if word_length - 1 >= W:  # 단어의 길이가 페이지 폭을 넘어가면 안됨
             break
         curr_penalty = dp[j - 2] + (W - word_length) ** 3
-        min_penalty = min(min_penalty, curr_penalty)
-    dp[i] = min_penalty
+        min_penalty = min(min_penalty, curr_penalty)  # 순차적으로 비교하며 최소 penalty 값을 찾아줌
+    dp[i] = min_penalty  # dp에 최소 penalty 값을 저장해줌
 
-print(dp[-1])
-
+print(dp[-1])  # dp의 마지막 값을 출력
 '''
 알고리즘 설명 및 수행시간 분석
 
@@ -26,6 +25,4 @@ dp[i]에는 i번째 단어까지의 최소 패널티를 저장해주어 결과�
 
 이 코드에서 가장 수행시간에 영향을 미치는 것은 이중 for문이므로 이 코드의 수행시간을 점화식으로 표현하자면
 T(n)=Cn^2+D(C와 D는 상수)이므로 수행시간을 Big-O로 표기하면 O(n^2)이다.
-
-
 '''
