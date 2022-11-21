8c38a11d809e0d160e9669ace4ab566536363dc9
박세현 <sarah1918@naver.com>
2022-11-21 22:24:27 +0900

    .

--- Sehyun/problem/한 개의 못 최대 막대 개수.py
+++ Sehyun/problem/한 개의 못 최대 막대 개수.py
@@ -1,22 +1,21 @@
-n = int(input())
+import sys
+
+n = int(sys.stdin.readline())
 lst=list()
 
 for _ in range(n):
-    a, b = map(int, input().split())
+    a, b = map(int, sys.stdin.readline().split())
     lst.append((a,"f"))
     lst.append((b, "l"))
 
 lst.sort(key=lambda x:(x[0],x[1]))
 
-
 cnt,max_cnt=0,0
-
-for i in range(n):
+for i in range(2*n):
     if lst[i][1]=="f":
         cnt+=1
-    elif lst[i][1]=="l":
-        max_cnt = max(cnt, max_cnt)
+    if lst[i][1]=="l":
         cnt-=1
-
+    max_cnt=max(cnt,max_cnt)
 
 print(max_cnt)
