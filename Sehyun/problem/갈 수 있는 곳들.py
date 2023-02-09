2efa30adc97b3fb51598c9b137696c4b318387af
박세현 <sarah1918@naver.com>
2023-02-09 14:53:14 +0900

    .

--- Sehyun/problem/갈 수 있는 곳들.py
+++ Sehyun/problem/갈 수 있는 곳들.py
@@ -1,10 +1,13 @@
 from collections import deque
+
 n,k=map(int,input().split())
 grid=[list(map(int,input().split())) for _ in range(n)]
 visited=[[0]*n for _ in range(n)]
 q=deque()
+
 def in_range(x,y):
     return 0<=x and x<n and 0<=y and y<n
+
 def can_go(x,y):
     if not in_range(x,y):
         return False
@@ -12,23 +15,28 @@ def can_go(x,y):
         return False
     return True
 
+
 def bfs():
-    dxs,dys=[-1,1,0,0],[0,0,-1,1]
     while q:
         x,y=q.popleft()
+        dxs,dys=[-1,1,0,0],[0,0,-1,1]
         for dx,dy in zip(dxs,dys):
             nx,ny=x+dx,y+dy
             if can_go(nx,ny):
-                visited[nx][ny]=True
+                visited[nx][ny]=1
                 q.append((nx,ny))
 
 for _ in range(k):
     x,y=map(int,input().split())
     q.append((x-1,y-1))
-    visited[x-1][y-1]=True
+    visited[x-1][y-1]=1
+
 bfs()
-print(sum([1
-          for i in range(n)
-          for j in range(n)
-          if visited[i][j]]))
+cnt=0
+
+for i in range(n):
+    for j in range(n):
+        cnt+=visited[i][j]
+
+print(cnt)
 
--- Sehyun/problem/최소 경로로 탈출 하기.py
+++ Sehyun/problem/최소 경로로 탈출 하기.py
@@ -1,28 +1,41 @@
 from collections import deque
+
 n,m=map(int,input().split())
-grid=[list(map(int,input().split())) for _ in range(n)]
+
+grid=[
+    list(map(int,input().split()))
+    for _ in range(n)
+]
 
 q=deque()
-step=[[0]*m for _ in range(n)]
-visited=[[False]*m for _ in range(n)]
+step=[
+    [0]*m
+    for _ in range(n)
+]
+visited=[
+    [0]*m
+    for _ in range(n)
+]
+
+def in_range(x,y):
+    return 0<=x and x<n and 0<=y and y<m
 
 def push(x,y,s):
     step[x][y]=s
     visited[x][y]=True
     q.append((x,y))
-def in_range(x,y):
-    return 0<=x and x<n and 0<=y and y<m
+
 def bfs():
-    dxs,dys=[-1,1,0,0],[0,0,-1,1]
     while q:
         x,y=q.popleft()
+        dxs,dys=[-1,1,0,0],[0,0,-1,1]
         for dx,dy in zip(dxs,dys):
             nx,ny=x+dx,y+dy
-            if in_range(nx,ny) and not visited[nx][ny] and grid[nx][ny]:
-                push(nx,ny,step[x][y]+1)
+            if in_range(nx, ny) and not visited[nx][ny] and grid[nx][ny]:
+                push(nx, ny, step[x][y] + 1)
+
 push(0,0,0)
 bfs()
-
 if visited[n-1][m-1]:
     print(step[n-1][m-1])
 else:
