d57a18915fb0803bbc583f8c6588aaa6ef378366
박세현 <sarah1918@naver.com>
2023-04-29 00:11:47 +0900

    .

--- Sehyun/problem/2805.py
+++ Sehyun/problem/2805.py
@@ -1,23 +1,24 @@
 import sys
 
-def binary_search(array,height,target,start,end):
-    while start<=end:
-        mid=(start+end)//2
-        print(sum([x-array[mid] for x in height if x>array[mid]]),array[mid],target)
-        if target==sum([x-array[mid] for x in height if x>array[mid]]):
-            return array[mid]
-        elif sum([x-array[mid] for x in height if x>array[mid]])>target:
-            start=mid+1
-        else:
-            end=mid-1
-    return None
+def binary_search(height,target,start,end):
+    mid = (start + end) // 2
+    if start>end:
+        return mid
+    cnt=0
+    for tree in height:
+        if tree>mid:
+            cnt+=(tree-mid)
+    if cnt<target:
+        return binary_search(height,target,start,mid-1)
+    elif cnt>=target:
+        return binary_search(height,target,mid+1,end)
+
+
 
 n,m=map(int,sys.stdin.readline().split())
 height=list(map(int,sys.stdin.readline().split()))
-lst=[x for x in range(1,max(height))]
-lst.sort()
-print(lst)
 
 
-print(binary_search(lst,height,m,0,len(lst)-1))
+
+print(binary_search(height,m,0,max(height)))
 
